7606a4d3196387e661530a383721853b8f13fab4
蓝白 <820232125@qq.com>
2023-03-13 23:05:12 +0800

    注释redis

--- crabc-boot/crabc-admin/src/main/java/cn/crabc/core/admin/config/RedisConfig.java
+++ crabc-boot/crabc-admin/src/main/java/cn/crabc/core/admin/config/RedisConfig.java
@@ -1,123 +1,123 @@
-package cn.crabc.core.admin.config;
-
-import cn.crabc.core.admin.component.RedisClient;
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisPassword;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.time.Duration;
-
-/**
- *  redis配置
- *
- * @author yuqf
- */
-@Configuration
-@ConditionalOnProperty(name = "crabc.cache.type", havingValue = "redis")
-public class RedisConfig {
-    @Value("${spring.redis.database}")
-    private int database;
-
-    @Value("${spring.redis.host}")
-    private String host;
-
-    @Value("${spring.redis.password}")
-    private String password;
-
-    @Value("${spring.redis.port}")
-    private int port;
-
-    @Value("${spring.redis.timeout}")
-    private long timeout;
-
-    @Value("${spring.redis.lettuce.shutdown-timeout}")
-    private long shutDownTimeout;
-
-    @Value("${spring.redis.lettuce.pool.max-idle}")
-    private int maxIdle;
-
-    @Value("${spring.redis.lettuce.pool.min-idle}")
-    private int minIdle;
-
-    @Value("${spring.redis.lettuce.pool.max-active}")
-    private int maxActive;
-
-    @Value("${spring.redis.lettuce.pool.max-wait}")
-    private long maxWait;
-
-    Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-
-    @Bean
-    public LettuceConnectionFactory lettuceConnectionFactory() {
-        GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
-        genericObjectPoolConfig.setMaxIdle(maxIdle);
-        genericObjectPoolConfig.setMinIdle(minIdle);
-        genericObjectPoolConfig.setMaxTotal(maxActive);
-        genericObjectPoolConfig.setMaxWaitMillis(maxWait);
-        genericObjectPoolConfig.setTimeBetweenEvictionRunsMillis(100);
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setDatabase(database);
-        redisStandaloneConfiguration.setHostName(host);
-        redisStandaloneConfiguration.setPort(port);
-        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
-        LettuceClientConfiguration clientConfig = LettucePoolingClientConfiguration.builder()
-                .commandTimeout(Duration.ofMillis(timeout))
-                .shutdownTimeout(Duration.ofMillis(shutDownTimeout))
-                .poolConfig(genericObjectPoolConfig)
-                .build();
-
-        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisStandaloneConfiguration, clientConfig);
-//        factory.setShareNativeConnection(true);
-//        factory.setValidateConnection(false);
-        return factory;
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(lettuceConnectionFactory);
-        //使用Jackson2JsonRedisSerializer替换默认的JdkSerializationRedisSerializer来序列化和反序列化redis的value值
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        mapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
-                ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
-        jackson2JsonRedisSerializer.setObjectMapper(mapper);
-        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        //key采用String的序列化方式
-        template.setKeySerializer(stringRedisSerializer);
-        // hash的key也采用String的序列化方式
-        template.setHashKeySerializer(stringRedisSerializer);
-        // value序列化方式采用jackson
-        template.setValueSerializer(jackson2JsonRedisSerializer);
-        // hash的value序列化方式采用jackson
-        template.setHashValueSerializer(jackson2JsonRedisSerializer);
-        template.afterPropertiesSet();
-        return template;
-    }
-
-    /**
-     * Redis缓存
-     * @param redisTemplate
-     * @return
-     */
-    @Bean
-    public RedisClient redisClient(RedisTemplate redisTemplate) {
-        return new RedisClient(redisTemplate);
-    }
-}
-
+//package cn.crabc.core.admin.config;
+//
+//import cn.crabc.core.admin.component.RedisClient;
+//import com.fasterxml.jackson.annotation.JsonAutoDetect;
+//import com.fasterxml.jackson.annotation.JsonTypeInfo;
+//import com.fasterxml.jackson.annotation.PropertyAccessor;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+//import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisPassword;
+//import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+//import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+//import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+//import org.springframework.data.redis.serializer.StringRedisSerializer;
+//
+//import java.time.Duration;
+//
+///**
+// *  redis配置
+// *
+// * @author yuqf
+// */
+//@Configuration
+//@ConditionalOnProperty(name = "crabc.cache.type", havingValue = "redis")
+//public class RedisConfig {
+//    @Value("${spring.redis.database}")
+//    private int database;
+//
+//    @Value("${spring.redis.host}")
+//    private String host;
+//
+//    @Value("${spring.redis.password}")
+//    private String password;
+//
+//    @Value("${spring.redis.port}")
+//    private int port;
+//
+//    @Value("${spring.redis.timeout}")
+//    private long timeout;
+//
+//    @Value("${spring.redis.lettuce.shutdown-timeout}")
+//    private long shutDownTimeout;
+//
+//    @Value("${spring.redis.lettuce.pool.max-idle}")
+//    private int maxIdle;
+//
+//    @Value("${spring.redis.lettuce.pool.min-idle}")
+//    private int minIdle;
+//
+//    @Value("${spring.redis.lettuce.pool.max-active}")
+//    private int maxActive;
+//
+//    @Value("${spring.redis.lettuce.pool.max-wait}")
+//    private long maxWait;
+//
+//    Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+//
+//    @Bean
+//    public LettuceConnectionFactory lettuceConnectionFactory() {
+//        GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
+//        genericObjectPoolConfig.setMaxIdle(maxIdle);
+//        genericObjectPoolConfig.setMinIdle(minIdle);
+//        genericObjectPoolConfig.setMaxTotal(maxActive);
+//        genericObjectPoolConfig.setMaxWaitMillis(maxWait);
+//        genericObjectPoolConfig.setTimeBetweenEvictionRunsMillis(100);
+//        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+//        redisStandaloneConfiguration.setDatabase(database);
+//        redisStandaloneConfiguration.setHostName(host);
+//        redisStandaloneConfiguration.setPort(port);
+//        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+//        LettuceClientConfiguration clientConfig = LettucePoolingClientConfiguration.builder()
+//                .commandTimeout(Duration.ofMillis(timeout))
+//                .shutdownTimeout(Duration.ofMillis(shutDownTimeout))
+//                .poolConfig(genericObjectPoolConfig)
+//                .build();
+//
+//        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisStandaloneConfiguration, clientConfig);
+////        factory.setShareNativeConnection(true);
+////        factory.setValidateConnection(false);
+//        return factory;
+//    }
+//
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+//        RedisTemplate<String, Object> template = new RedisTemplate<>();
+//        template.setConnectionFactory(lettuceConnectionFactory);
+//        //使用Jackson2JsonRedisSerializer替换默认的JdkSerializationRedisSerializer来序列化和反序列化redis的value值
+//        ObjectMapper mapper = new ObjectMapper();
+//        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+//        mapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
+//                ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+//        jackson2JsonRedisSerializer.setObjectMapper(mapper);
+//        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+//        //key采用String的序列化方式
+//        template.setKeySerializer(stringRedisSerializer);
+//        // hash的key也采用String的序列化方式
+//        template.setHashKeySerializer(stringRedisSerializer);
+//        // value序列化方式采用jackson
+//        template.setValueSerializer(jackson2JsonRedisSerializer);
+//        // hash的value序列化方式采用jackson
+//        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+//        template.afterPropertiesSet();
+//        return template;
+//    }
+//
+//    /**
+//     * Redis缓存
+//     * @param redisTemplate
+//     * @return
+//     */
+//    @Bean
+//    public RedisClient redisClient(RedisTemplate redisTemplate) {
+//        return new RedisClient(redisTemplate);
+//    }
+//}
+//
