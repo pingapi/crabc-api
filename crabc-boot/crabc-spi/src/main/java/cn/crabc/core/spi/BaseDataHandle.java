c07c75ad2405bf48b636699a05de841ef8e3fadb
蓝白 <820232125@qq.com>
2023-03-19 21:07:51 +0800

    添加注释

--- crabc-boot/crabc-spi/src/main/java/cn/crabc/core/spi/BaseDataHandle.java
+++ crabc-boot/crabc-spi/src/main/java/cn/crabc/core/spi/BaseDataHandle.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023, crabc.cn (creabc@qq.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package cn.crabc.core.spi;
 
 import java.util.List;
@@ -9,15 +24,65 @@ import java.util.List;
  */
 public interface BaseDataHandle<T> {
 
+    /**
+     * 查询单个对象
+     * @param dataSourceId
+     * @param schema
+     * @param sql
+     * @param params
+     * @return
+     */
     T selectOne(String dataSourceId, String schema, String sql, Object params);
 
+    /**
+     * 查询对象列表
+     * @param dataSourceId
+     * @param schema
+     * @param sql
+     * @param params
+     * @return
+     */
     List<T> selectList(String dataSourceId, String schema, String sql, Object params);
 
+    /**
+     * 查询分页对象
+     * @param dataSourceId
+     * @param schema
+     * @param sql
+     * @param params
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
     Object selectPage(String dataSourceId, String schema, String sql, Object params, int pageNum, int pageSize);
 
+    /**
+     * 新增
+     * @param dataSourceId
+     * @param schema
+     * @param sql
+     * @param params
+     * @return
+     */
     int insert(String dataSourceId, String schema, String sql, Object params);
 
+    /**
+     * 删除
+     * @param dataSourceId
+     * @param schema
+     * @param sql
+     * @param params
+     * @return
+     */
     int delete(String dataSourceId, String schema, String sql, Object params);
 
+    /**
+     * 修改
+     * @param dataSourceId
+     * @param schema
+     * @param sql
+     * @param params
+     * @return
+     */
     int update(String dataSourceId, String schema, String sql, Object params);
 }
--- crabc-boot/crabc-spi/src/main/java/cn/crabc/core/spi/DataSourceDriver.java
+++ crabc-boot/crabc-spi/src/main/java/cn/crabc/core/spi/DataSourceDriver.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023, crabc.cn (creabc@qq.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package cn.crabc.core.spi;
 
 import cn.crabc.core.spi.bean.BaseDataSource;
@@ -14,19 +29,57 @@ import java.util.List;
  */
 public interface DataSourceDriver<T> extends BaseDataHandle<T> {
 
+    /**
+     * 数据源名称
+     * @return
+     */
     String getName();
 
+    /**
+     * 测试数据源连接
+     * @param dataSource
+     * @return
+     */
     Integer test(BaseDataSource dataSource);
 
+    /**
+     * 初始化
+     * @param dataSource
+     */
     void init(BaseDataSource dataSource);
 
+    /**
+     * 销毁
+     * @param dataSourceId
+     */
     void destroy(String dataSourceId);
 
     List<String> getCatalogs(String dataSourceId);
 
+    /**
+     * 获取数据源Schema
+     * @param dataSourceId
+     * @param catalog
+     * @return
+     */
     List<Schema> getSchemas(String dataSourceId, String catalog);
 
+    /**
+     * 获取数据表列表
+     * @param dataSourceId
+     * @param catalog
+     * @param schema
+     * @return
+     */
     List<Table> getTables(String dataSourceId, String catalog, String schema);
 
+    /**
+     * 获取数据表字段列表
+     * @param dataSourceId
+     * @param catalog
+     * @param schema
+     * @param table
+     * @return
+     */
     List<Column> getColumns(String dataSourceId, String catalog, String schema, String table);
 }
