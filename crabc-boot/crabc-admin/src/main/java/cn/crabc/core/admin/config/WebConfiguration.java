2fd5a94078aea1354ed8c6b499298050a63d76d7
蓝白 <820232125@qq.com>
2023-03-22 00:13:29 +0800

    修改配置

--- crabc-boot/crabc-admin/src/main/java/cn/crabc/core/admin/config/WebConfiguration.java
+++ crabc-boot/crabc-admin/src/main/java/cn/crabc/core/admin/config/WebConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -19,27 +18,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfiguration implements WebMvcConfigurer {
 
     /**
-     * 跳转
+     * 跳转首页
+     *
      * @return
      */
     @Bean
-    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer(){
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
         return factory -> {
             ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/index.html");
             factory.addErrorPages(error404Page);
         };
-    }
-
-    @Override
-    public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedHeaders("Access-Control-Allow-Origin",
-                        "*",
-                        "Access-Control-Allow-Methods",
-                        "POST, GET, OPTIONS, PUT, DELETE",
-                        "Access-Control-Allow-Headers",
-                        "Origin, X-Requested-With, Content-Type, Accept")
-                .allowedOrigins("*")
-                .allowedMethods("*");
     }
 }
