934b317511a06569179a52c542584c61f45e4844
蓝白 <iamyuqf@163.com>
2023-03-28 14:30:05 +0800

    新增mongodb模块

---
+++ crabc-boot/crabc-plugin/mongodb-connector/src/main/java/cn/crabc/core/plugin/mongodb/MongodbDataSourceDriver.java
@@ -0,0 +1,44 @@
+package cn.crabc.core.plugin.mongodb;
+
+import cn.crabc.core.spi.DataSourceDriver;
+import cn.crabc.core.spi.MetaDataMapper;
+import cn.crabc.core.spi.StatementMapper;
+import cn.crabc.core.spi.bean.BaseDataSource;
+
+/**
+ * mongodb 驱动实现
+ *
+ * @author yuqf
+ */
+public class MongodbDataSourceDriver implements DataSourceDriver {
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Integer test(BaseDataSource dataSource) {
+        return null;
+    }
+
+    @Override
+    public void init(BaseDataSource dataSource) {
+
+    }
+
+    @Override
+    public void destroy(String dataSourceId) {
+
+    }
+
+    @Override
+    public MetaDataMapper getMetaData() {
+        return null;
+    }
+
+    @Override
+    public StatementMapper getStatement() {
+        return null;
+    }
+}
