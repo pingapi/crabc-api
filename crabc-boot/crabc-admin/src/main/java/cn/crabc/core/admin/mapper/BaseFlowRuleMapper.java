f8a97ac548aac6caa1cba9d45e6cb488f6d0c7e0
蓝白 <iamyuqf@163.com>
2023-05-20 10:04:19 +0800

    限流规则操作

---
+++ crabc-boot/crabc-admin/src/main/java/cn/crabc/core/admin/mapper/BaseFlowRuleMapper.java
@@ -0,0 +1,70 @@
+package cn.crabc.core.admin.mapper;
+
+import cn.crabc.core.admin.entity.BaseFlowRule;
+import cn.crabc.core.admin.entity.vo.GroupApiVO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 限流策略 Mapper接口
+ *
+ * @author yuqf
+ */
+public interface BaseFlowRuleMapper {
+
+
+    /**
+     * 查询规则列表
+     * @return
+     */
+    List<BaseFlowRule> selectList(@Param("flowName") String flowName);
+
+    /**
+     * 获取缓存限流api
+     * @return
+     */
+    List<BaseFlowRule> selectFlows();
+
+    /**
+     * 获取分组下限流API
+     * @return
+     */
+    List<GroupApiVO> selectGroupApi(@Param("flowId") Integer flowId);
+
+    /**
+     * 添加规则
+     * @param rule
+     * @return
+     */
+    Integer insert(BaseFlowRule rule);
+
+    /**
+     * 删除
+     * @param flowId
+     * @return
+     */
+    Integer delete(@Param("flowId") Integer flowId);
+
+    /**
+     * 编辑
+     * @param rule
+     * @return
+     */
+    Integer update(BaseFlowRule rule);
+
+    /**
+     * 新增关联API
+     * @param flowId
+     * @param list
+     * @return
+     */
+    Integer insertFlowApi(@Param("flowId") Integer flowId, @Param("userId") String userId, @Param("list") List<Integer> list);
+
+    /**
+     * 删除关联API
+     * @param flowId
+     * @return
+     */
+    Integer deleteFlowApi(@Param("flowId") Integer flowId);
+}
---
+++ crabc-boot/crabc-admin/src/main/java/cn/crabc/core/admin/service/system/impl/BaseFlowRuleServiceImpi.java
@@ -0,0 +1,133 @@
+package cn.crabc.core.admin.service.system.impl;
+
+import cn.crabc.core.admin.entity.BaseFlowRule;
+import cn.crabc.core.admin.entity.vo.FlowApiVO;
+import cn.crabc.core.admin.entity.vo.GroupApiVO;
+import cn.crabc.core.admin.mapper.BaseFlowRuleMapper;
+import cn.crabc.core.admin.mapper.BaseGroupMapper;
+import cn.crabc.core.admin.service.system.IBaseFlowRuleService;
+import cn.crabc.core.admin.util.PageInfo;
+import cn.crabc.core.admin.util.UserThreadLocal;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import com.github.pagehelper.PageHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * 限流策略 服务实现
+ *
+ * @author yuqf
+ */
+@Service
+public class BaseFlowRuleServiceImpi implements IBaseFlowRuleService {
+
+    @Autowired
+    private BaseFlowRuleMapper baseFlowRuleMapper;
+    @Autowired
+    private BaseGroupMapper baseGroupMapper;
+
+    @Scheduled(cron = "*/50 * * * * ?")
+    public void task() {
+        initFlowRule();
+    }
+
+    @Override
+    public void initFlowRule() {
+        // 加载限流规则
+        List<FlowRule> rules = new ArrayList<>();
+        List<BaseFlowRule> flowList = this.getCacheFlowList();
+        for (BaseFlowRule flow : flowList) {
+            FlowRule rule = new FlowRule();
+            rule.setResource("/api/web/"+flow.getApiPath());
+            rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+            rule.setCount(flow.getFlowCount());
+            rule.setId(flow.getApiId());
+            rules.add(rule);
+        }
+        FlowRuleManager.loadRules(rules);
+    }
+
+    @Override
+    public List<BaseFlowRule> getCacheFlowList() {
+        return baseFlowRuleMapper.selectFlows();
+    }
+
+    @Override
+    public PageInfo<BaseFlowRule> getFlowPage(String flowName, Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<BaseFlowRule> list = baseFlowRuleMapper.selectList(flowName);
+        return new PageInfo<>(list, pageNum, pageSize);
+    }
+
+    @Override
+    public FlowApiVO getFlowAuthApi(Integer flowId) {
+        FlowApiVO flowApi = new FlowApiVO();
+        List<GroupApiVO> groupAllApi = baseGroupMapper.selectGroupApi();
+        List<GroupApiVO> groupApi = baseFlowRuleMapper.selectGroupApi(flowId);
+        // 删除已授权的API
+        groupAllApi.removeAll(groupApi);
+        flowApi.setLeft(this.getChildApi(groupAllApi));
+        flowApi.setRight(this.getChildApi(groupApi));
+        return flowApi;
+    }
+
+    /**
+     * 获取分组子集API
+     * @param groupApiList
+     * @return
+     */
+    private List<GroupApiVO> getChildApi(List<GroupApiVO> groupApiList){
+        Map<Integer, List<GroupApiVO>> groupMap = groupApiList.stream().collect(Collectors.groupingBy(GroupApiVO::getGroupId));
+        List<GroupApiVO> list = new ArrayList<>();
+        for (Integer groupId : groupMap.keySet()) {
+            List<GroupApiVO> groupApis = groupMap.get(groupId);
+            GroupApiVO parentGroup = new GroupApiVO();
+            parentGroup.setGroupId(groupId);
+            parentGroup.setGroupName(groupApis.get(0).getGroupName());
+            parentGroup.setChild(groupApis);
+            list.add(parentGroup);
+        }
+        return list;
+    }
+
+    @Override
+    public Integer deleteFlow(Integer flowId) {
+        return baseFlowRuleMapper.delete(flowId);
+    }
+
+    @Override
+    public Integer addFlowRule(BaseFlowRule flowRule) {
+        String userId = UserThreadLocal.getUserId();
+        Date date = new Date();
+        flowRule.setCreateBy(userId);
+        flowRule.setCreateTime(date);
+        flowRule.setUpdateTime(date);
+        flowRule.setFlowType("flow");
+        return baseFlowRuleMapper.insert(flowRule);
+    }
+
+    @Override
+    public Integer updateFlowRule(BaseFlowRule flowRule) {
+        String userId = UserThreadLocal.getUserId();
+        Date date = new Date();
+        flowRule.setUpdateBy(userId);
+        flowRule.setUpdateTime(date);
+        return baseFlowRuleMapper.update(flowRule);
+    }
+
+    @Override
+    public Integer addFlowApi(Integer flowId, List<Integer> apiIds) {
+        String userId = UserThreadLocal.getUserId();
+        baseFlowRuleMapper.deleteFlowApi(flowId);
+        return baseFlowRuleMapper.insertFlowApi(flowId, userId, apiIds);
+    }
+}
