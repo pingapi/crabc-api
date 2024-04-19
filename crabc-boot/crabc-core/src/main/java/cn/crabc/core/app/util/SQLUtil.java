cbf7cdbaefb9030915415959469b286f2aebbeb9
蓝白 <iamyuqf@163.com>
2024-04-20 00:18:36 +0800

    新增SQL模板

--- crabc-boot/crabc-core/src/main/java/cn/crabc/core/app/util/SQLUtil.java
+++ crabc-boot/crabc-core/src/main/java/cn/crabc/core/app/util/SQLUtil.java
@@ -391,4 +391,29 @@ public class SQLUtil {
         }
         return "custom";
     }
+
+    /**
+     * 字符串下划线转驼峰
+     * @param param
+     * @return
+     */
+    public static String underlineToCamel(String param){
+        if (param==null||"".equals(param.trim())){
+            return "";
+        }
+        int len=param.length();
+        StringBuilder sb=new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            char c = Character.toLowerCase(param.charAt(i));
+            if (c == '_'){
+                if (++i<len){
+                    sb.append(Character.toUpperCase(param.charAt(i)));
+                }
+            }else{
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
 }
