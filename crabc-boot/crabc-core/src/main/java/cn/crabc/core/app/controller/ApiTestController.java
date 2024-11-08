b57287ceeac4d0c7da24a89248678de764b9c1f3
蓝白 <iamyuqf@163.com>
2024-11-08 14:16:18 +0800

    优化参数校验

--- crabc-boot/crabc-core/src/main/java/cn/crabc/core/app/controller/ApiTestController.java
+++ crabc-boot/crabc-core/src/main/java/cn/crabc/core/app/controller/ApiTestController.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -68,10 +69,18 @@ public class ApiTestController {
             Object requestParams = params.getRequestParams();
             if (requestParams instanceof Map) {
                 Map<String, Object> queryParam = (Map<String, Object>) requestParams;
+                // 校验参数
+                if (!splitAarry(queryParam)) {
+                    return Result.error(ErrorStatusEnum.PARAM_NOT_FOUNT.getCode(), ErrorStatusEnum.PARAM_NOT_FOUNT.getMassage());
+                }
                 paramsMap.putAll(queryParam);
             }else if (requestParams instanceof List) {
                 List<Map<String,Object>> paramsList = (List<Map<String, Object>>) requestParams;
                 for (Map<String, Object> entry : paramsList) {
+                    // 校验参数
+                    if (!splitAarry(entry)) {
+                        return Result.error(ErrorStatusEnum.PARAM_NOT_FOUNT.getCode(), ErrorStatusEnum.PARAM_NOT_FOUNT.getMassage());
+                    }
                     paramsMap.put(entry.get("name").toString(), entry.get("value"));
                 }
             }
@@ -89,5 +98,27 @@ public class ApiTestController {
             return Result.error("测试异常，请检查参数或者SQL是否正常！");
         }
         return Result.success(resultMap);
+    }
+    /**
+     * 校验参数
+     *
+     * @param queryParam
+     * @return
+     */
+    public boolean splitAarry(Map<String, Object> queryParam) {
+        if (queryParam == null ||queryParam.isEmpty()) {
+            return true;
+        }
+        String paramType = queryParam.get("paramType")+"";
+        Object paramValue = queryParam.get("value");
+
+        // Array类型的拼接参数 进行拆分
+        if ("Array".equalsIgnoreCase(paramType) && paramValue != null) {
+            String[] values = paramValue.toString().split(",");
+            queryParam.put("value", Arrays.asList(values));
+        }else if ("Array".equalsIgnoreCase(paramType) && "".equals(paramValue)) {
+            return false;
+        }
+        return true;
     }
 }
