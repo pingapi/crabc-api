b2dc24cab58af297ee13c87875837b3f72ca5a30
蓝白 <iamyuqf@163.com>
2026-05-23 13:24:51 +0800

    更新版本5.0

--- db/update.sql
+++ db/update.sql
@@ -1,3 +1,6 @@
+-- v4.5.0
+ALTER TABLE base_api_info ADD draft_content longtext COMMENT '接口暂存内容';
+-- v5.0.0
 ALTER TABLE base_api_info ADD transaction_enabled int DEFAULT 0 COMMENT '是否开启事务 1/0';
 ALTER TABLE base_api_info ADD rate_limit_window_seconds int DEFAULT NULL COMMENT '限流时间窗口秒数';
 ALTER TABLE base_api_info ADD rate_limit_count int DEFAULT NULL COMMENT '限流窗口请求次数';
--- db/v4.5.0_update.sql
+++
@@ -1,2 +0,0 @@
--- v4.5.0
-ALTER TABLE base_api_info ADD draft_content longtext COMMENT '接口暂存内容';
